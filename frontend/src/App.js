fe858990f9b49f6b65925f7bccaccb628e083e01
hrishav77 <f20210629@goa.bits-pilani.ac.in>
2023-07-16 12:59:25 +0530

    uploading the frontend
    uploaded the frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+
+import InputForm from './Form';
+
+
+function App() {
+
+  return (
+   <InputForm/>
+  );
+}
+
+export default App;
---
+++ frontend/src/Form.js
@@ -0,0 +1,187 @@
+import { useState } from 'react';
+import { ChakraProvider, Box, FormControl, FormLabel, Input, Button,Select,extendTheme,Text, Center} from '@chakra-ui/react';
+
+const InputForm = () => {
+    const theme = extendTheme({
+        styles: {
+          global: () => ({
+            body: {
+              bg: ""
+            }
+          })
+        }
+      });
+
+    const [pred,setpred]=useState("")
+    const [formData, setFormData] = useState({
+        'Age': '',
+        'Sleep Duration': '',
+        'Quality of Sleep(4-10)': '',
+        'Physical Activity Level(30-100)': '',
+        'Stress Level(3-8)': '',
+        'Heart Rate': '',
+        'Daily Steps(max 10000)': '',
+        'BloodPressure_high': '',
+        'BloodPressure_low': '',
+        'Gender_Female(1 or 0)': '',
+        'Gender_Male(1 or 0)': '',
+        'Occupation_Accountant': '0',
+        'Occupation_Doctor': '0',
+        'Occupation_Engineer': '0',
+        'Occupation_Lawyer': '0',
+        'Occupation_Manager': '0',
+        'Occupation_Nurse': '0',
+        'Occupation_Sales Representative': '0',
+        'Occupation_Salesperson': '0',
+        'Occupation_Scientist': '0',
+        'Occupation_Software Engineer': '0',
+        'Occupation_Teacher': '0',
+        'BMI Category_Normal': '0',
+        'BMI Category_Normal Weight': '0',
+        'BMI Category_Obese': '0',
+        'BMI Category_Overweight': '0',
+      });
+
+      const handleSubmit =async (e) => {
+        e.preventDefault();
+        const updatedFormData = { ...formData };
+        setFormData(updatedFormData);
+        try {
+          const response = await fetch('http://127.0.0.1:4000/form', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+          });
+      
+          if (response.ok) {
+            const prediction = await response.json(); // Extract the JSON data from the response
+            setpred(prediction)
+          } else {
+            console.error('Error submitting form');
+            // Handle error response
+          }
+        } catch (error) {
+          console.error('Error submitting form:', error);
+          // Handle error case
+        }
+
+        console.log(formData);
+      };
+
+      const handleChange = (e) => {
+        const { value } = e.target;
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          [value]: '1',
+        }));
+      };
+
+
+      const selected_columns = [
+        'Age',
+        'Sleep Duration',
+        'Quality of Sleep(4-10)',
+        'Physical Activity Level(30-100)',
+        'Stress Level(3-8)',
+        'Heart Rate',
+        'Daily Steps(max 10000)',
+        'BloodPressure_high',
+        'BloodPressure_low',
+        'Gender_Female(1 or 0)',
+        'Gender_Male(1 or 0)',
+      ];
+      const occupationOptions = [
+        'Accountant',
+        'Doctor',
+        'Engineer',
+        'Lawyer',
+        'Manager',
+        'Nurse',
+        'Sales Representative',
+        'Salesperson',
+        'Scientist',
+        'Software Engineer',
+        'Teacher',
+      ];
+   
+      const changehandler = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevFormData) => ({
+          ...prevFormData,
+          [name]: value,
+        }));
+      };
+  
+
+  return (
+    <ChakraProvider theme={theme}>
+      {
+        !pred &&  <>
+        <Center><Text fontSize="3xl">Fill this form to know if you have any sleep disorder</Text></Center>
+        
+        <Box maxWidth="500px" margin="0 auto" mt={8} bg="white" p="5" borderRadius="5" boxShadow="lg">
+        <form onSubmit={handleSubmit}>
+          {selected_columns.map((column) => (
+            <FormControl key={column} mb={4}>
+              <FormLabel>{column}</FormLabel>
+              <Input
+                type="text"
+                name={column}
+                value={formData[column]}
+                onChange={changehandler}
+              />
+            </FormControl>
+          ))}
+           <FormControl mb={4}>
+            <FormLabel>Occupation</FormLabel>
+            <Select name="Occupation" onChange={handleChange} >
+              <option value="">Select an occupation</option>
+              {occupationOptions.map((occupation) => (
+                <option key={occupation} value={`Occupation_${occupation}`}>
+                  {occupation}
+                </option>
+              ))}
+            </Select>
+          </FormControl>
+          <FormControl mb={4}>
+            <FormLabel>BMI</FormLabel>
+            <Select name="BMI"  onChange={handleChange} >
+              <option value="">Select you BMI</option>
+              <option value="BMI Category_Normal">Normal</option>
+              <option value="BMI Category_Overweight">Overweight</option>
+              <option value="BMI Category_Obese">Obese</option>
+              <option value="BMI Category_Normal Weight">not sure</option>
+
+              {/* Add more occupation options here */}
+            </Select>
+          </FormControl>
+   
+          <Button colorScheme="teal" type="submit">
+            Submit
+          </Button>
+        </form>
+      </Box>
+      </>
+      }
+      {pred ? (
+  <Center>
+  <Text fontSize="4xl">
+ 
+    {pred === 'Insomnia' ? (
+      'You have a high chance of insomnia'
+    ) : pred === 'Sleep Apnea' ? (
+      'You have a high chance of sleep apnea'
+    ) : (
+      'Yay! You have a very little chance of any sleep disorder'
+    )}
+  </Text>
+  </Center>
+) : null}
+     
+    </ChakraProvider>
+  );
+};
+
+export default InputForm;
