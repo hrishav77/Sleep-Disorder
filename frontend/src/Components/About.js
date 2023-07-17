5a79144d1998741b20c1131a33008afe400c89f9
hrishav77 <f20210629@goa.bits-pilani.ac.in>
2023-07-17 17:44:06 +0530

    changed notebook link
    Modified my notebook a bit so changed link

--- frontend/src/Components/About.js
+++ frontend/src/Components/About.js
@@ -8,7 +8,7 @@ export default function About() {
         <Center>
         <Flex direction='column'>
         <Text fontSize="lg">I did some analysis on different factors which causes sleep disorders here is a jupyter notebook link showing the analysis</Text>
-        <a href='https://notebooksharing.space/view/c5ab25e96a24715ce13fe1767005fff7eb10bd096b99e8d836e47f674d5dba44#displayOptions='>
+        <a href='https://notebooksharing.space/view/51a2cdf80dd1abe804201912270a33337761ee8473804f4a55cd1e2cc98c5351#displayOptions='>
            <Button colorScheme='yellow' size="sm">Notebook link</Button> 
         </a>
         </Flex>
