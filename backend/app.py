cfabc82e056f3de85da4cf2cfcab9070d2c07419
hrishav77 <f20210629@goa.bits-pilani.ac.in>
2025-02-01 23:52:21 +0530

    changed requirments and probabilty

--- backend/app.py
+++ backend/app.py
@@ -59,7 +59,7 @@ def chatbot_response():
     probs = torch.softmax(output, dim=1)
     prob = probs[0][predicted.item()]
 
-    if prob > 0.50:
+    if prob > 0.70:
         for intent in intents["intents"]:
             if tag == intent["tag"]:
                 return jsonify({"response": random.choice(intent["responses"])})
